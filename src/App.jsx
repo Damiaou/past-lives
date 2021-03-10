2dacc7027487c6cccaa185e4a2a9097d1e4d2959
Damiaou <damsauvagere@gmail.com>
2021-03-10 21:22:54 +0100

    Change style

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,52 @@
-import React, { useEffect, useState } from "react";
-import { Header } from "./components/Header/Header";
-import { ButtonDiscover } from "./components/ButtonDiscover/ButtonDiscover";
-import { PastLive } from "./components/PastLive/PastLive";
-import { getRandomAdjective, getRandomAnimal } from "./utils/fetchingApis";
-import "./styles.css";
+import React, { useEffect, useState } from 'react';
+import { Header } from './components/Header/Header';
+import { ButtonDiscover } from './components/ButtonDiscover/ButtonDiscover';
+import { PastLive } from './components/PastLive/PastLive';
+import { getRandomAdjective, getRandomAnimal } from './utils/fetchingApis';
+import './styles.css';
 
 export default function App() {
-  const [discovered, setDiscovered] = useState(false);
-  const [animal, setAnimal] = useState("");
-  const [adjective, setAdjective] = useState("");
-  const [loading, setLoading] = useState(false);
+	const [discovered, setDiscovered] = useState(false);
+	const [animal, setAnimal] = useState('');
+	const [adjective, setAdjective] = useState('');
+	const [loading, setLoading] = useState(false);
 
-  const toggleDiscovered = () => {
-    setDiscovered(!discovered);
-  };
+	const toggleDiscovered = () => {
+		setDiscovered(!discovered);
+	};
 
-  useEffect(() => {
-    setLoading(true);
-    getRandomAnimal().then((animal) => {
-      setAnimal(animal);
-    });
-    getRandomAdjective(animal).then((adjective) => {
-      setAdjective(adjective);
-    });
-    setLoading(false);
-  }, [discovered]);
-  // Does not include animals and adjective by choice infinite loop if you do.
+	useEffect(() => {
+		setLoading(true);
+		getRandomAnimal().then((animal) => {
+			setAnimal(animal);
+			getRandomAdjective(animal).then((adjective) => {
+				setAdjective(adjective);
+			});
+		});
 
-  const handleClick = () => {
-    toggleDiscovered();
-  };
+		setLoading(false);
+	}, [discovered]);
+	// Does not include animals and adjective by choice infinite loop if you do.
 
-  return (
-    <div className="main">
-      {discovered ? (
-        <>
-          <Header
-            title="Remember"
-            homeAction={handleClick}
-            discovered={discovered}
-          />
-          <PastLive animal={animal} adjective={adjective} loading={loading} />
-        </>
-      ) : (
-        <>
-          <Header title="Past Lives" />
-          <div className="discover-button-container">
-            <ButtonDiscover onClick={handleClick} />
-          </div>
-        </>
-      )}
-    </div>
-  );
+	const handleClick = () => {
+		toggleDiscovered();
+	};
+
+	return (
+		<div className="main">
+			{discovered ? (
+				<>
+					<Header title="Remember" homeAction={handleClick} discovered={discovered} />
+					<PastLive animal={animal} adjective={adjective} loading={loading} />
+				</>
+			) : (
+				<>
+					<Header title="Past Lives" />
+					<div className="discover-button-container">
+						<ButtonDiscover onClick={handleClick} />
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
